33c74685c79dac7a5b78a6248a6d6314eef75fd8
junie87 <arjun@1upvc.com>
2022-03-15 16:53:25 -0500

    Update bio.js

--- src/components/bio.js
+++ src/components/bio.js
@@ -1,6 +1,6 @@
  import * as React from "react"
  import { StaticImage } from "gatsby-plugin-image"
- 
+
  const Bio = () => {
   return (
     <div className="bio">
@@ -8,13 +8,12 @@
         className="bio-avatar"
         objectPosition="50% 50%"
         formats={["auto", "webp"]}
-        src="../../static/assets/gatsby-icon-100x100.png"
+        src="../../static/assets/superpower-logo-x200.png"
         quality={95}
-        alt="Profile Picture"         
+        alt="Profile Picture"
       />
     </div>
    )
  }
- 
+
  export default Bio
- 
