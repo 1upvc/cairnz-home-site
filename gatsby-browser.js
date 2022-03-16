f122bfce98a93f686c1e0ae97b68f4898a2bdf0f
junie87 <arjun@1upvc.com>
2022-03-16 00:32:44 -0500

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,7 +3,7 @@ export const onServiceWorkerUpdateReady = () => {
       `This application has been updated. ` +
         `Reload to display the latest version?`
     )
-  
+
     if (answer === true) {
       window.location.reload()
     }
